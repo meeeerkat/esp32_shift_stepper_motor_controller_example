6ce352119d2b0702ee5cb932ac2305ba7c42517d
Paul Suau <paul.suau@grenoble-inp.org>
2023-02-04 18:13:06 +0000

    Now using finished_movement_queue

--- main/main.c
+++ main/main.c
@@ -12,7 +12,15 @@ shift_stepper_motor_controller_t ssmc;
 
 void app_main(void) {
   shift_register__init(&sr, 16, 17, 18, 8);
-  shift_stepper_motor_controller__init(&ssmc, 1, &sr, 10);
+  shift_stepper_motor_controller__init(&ssmc, 1, &sr, 10, 5);
+
+  QueueHandle_t* queue = shift_stepper_motor_controller_finished_movement_queue(&ssmc);
   
   shift_stepper_motor_controller__move(&ssmc, 0, 1000);
+
+  uint8_t id;
+  if (xQueueReceive(*queue, &id, portMAX_DELAY))
+    printf("DONE\n");
+  else
+    printf("ERROR\n");
 }
